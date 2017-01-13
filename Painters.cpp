901e3944657b0da0270bd0ae7ce5933161582904
Enes Keles <eneskeles81@gmail.com>
2017-01-13 20:10:27 +0300

    Update Painters.cpp

--- Painters.cpp
+++ Painters.cpp
@@ -3,50 +3,51 @@
 
 #define INF 1<<30
 
-using namespace std ;
+using namespace std;
 
-int DP[25][25][505] ;
-int n, arr[505] ;
+int DP[25][25][505];
+int n, arr[505];
 
-int solve ( int c1, int c2, int i )
+int solve ( int c1, int c2, int i)
 {
-	if ( DP[c1][c2][i] != -1 )
-		return DP[c1][c2][i] ;
+	if ( DP[c1][c2][i] != -1)
+		return DP[c1][c2][i];
 
 	if ( c1 != arr[i] && c2 != arr[i])
-		return DP[c1][c2][i] = 1 + min( solve(arr[i],c2,i+1),solve(c1,arr[i],i+1)) ;
+		return DP[c1][c2][i] = 1 + min( solve(arr[i],c2,i+1),solve(c1,arr[i],i+1));
 
 	else if ( c1 == arr[i] || c2 == arr[i]) 
-		return DP[c1][c2][i] = solve(c1,c2,i+1) ;
+		return DP[c1][c2][i] = solve(c1,c2,i+1);
+	
 	return -1 ;
 }
 
 int main()
 {
-	int T ;
+	int T;
 
 
-	cin>> T;
+	cin>>T;
 
 	while ( T--)
 	{
 		cin>> n;
 
 		for ( int i = 1; i <= n; ++i)
-			cin>> arr[i] ;
+			cin>> arr[i];
 		
 		for ( int i = 0; i < 25; ++i)
 			for ( int j =0; j < 25; ++j)
 				for ( int k = 0; k < 505; ++k)
 				{
-					DP[i][j][k] = -1 ;
+					DP[i][j][k] = -1;
 					
 					if ( k == n+1) 
-						DP[i][j][k] = 0 ;
+						DP[i][j][k] = 0;
 				}
 		
 
-		cout << solve(21, 21, 1) << endl ; 
+		cout << solve(21, 21, 1) << endl; 
 	}
 	return 0;
 }
