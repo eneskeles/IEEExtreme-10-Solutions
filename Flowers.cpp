820f94c38fe7d6af333048c5c0a93ca62cd1d1e3
Enes Keles <eneskeles81@gmail.com>
2017-10-21 17:06:01 +0300

    Update Flowers.cpp

--- Flowers.cpp
+++ Flowers.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #include <cmath>
 using namespace std ;
-
 typedef long long unsigned llu ; 
 
 int main()
@@ -11,27 +10,24 @@ int main()
 
 	arr[0] = 1;
 
-	llu k,N;
+	llu k, N;
 
 	cin>>T;
 
-	for ( int i = 1; i <= 63 ; ++i)
+	for (int i = 1; i <= 63 ; ++i)
 		arr[i] = arr[i-1]*2; 
 
 	while (T--)
 	{
-		
 		cin>> N;
 		
 		int i ;
-		for ( i = 0 ; ; ++i)
-			if ( arr[i] > N)
+		for (i = 0 ; ; ++i)
+			if (arr[i] > N)
 				break; 
-		
 		--i;
 
-		cout << (long long)((N-arr[i])*2+1) << endl;
-	
+		cout<<(long long)((N - arr[i])*2 + 1)<<endl;
 	}
 
 	return 0;
